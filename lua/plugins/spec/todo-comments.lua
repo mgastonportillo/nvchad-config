a43b88abe7e71100d2f72be708c0ed795f4ddb4e
mgastonportillo <mgastonportillo@gmail.com>
2024-06-03 01:10:03 -0300

    fix(spec): incorrect lazyloading event for todo-comments

--- lua/plugins/spec/todo-comments.lua
+++ lua/plugins/spec/todo-comments.lua
@@ -1,7 +1,7 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = "nvim-lua/plenary.nvim",
-  event = "BufRead",
+  event = "VeryLazy",
   config = function()
     require("todo-comments").setup {
       keywords = {

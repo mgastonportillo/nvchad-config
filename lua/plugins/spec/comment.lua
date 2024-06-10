a12cb1ca81914cd5c8055c0703ba8d364e166188
mgastonportillo <mgastonportillo@gmail.com>
2024-06-10 19:56:37 -0300

    chore(spec): remove unnecessary configs in Comment plugin
    - scss and astro native support has been improved

--- lua/plugins/spec/comment.lua
+++ lua/plugins/spec/comment.lua
@@ -19,15 +19,10 @@ return {
       { desc = "Comment toggle (aware of context)" }
     )
   end,
-  config = function()
+  config = function(_, opts)
     local comment = require "Comment"
     local ts_addon = require "ts_context_commentstring.integrations.comment_nvim"
-
-    comment.setup { pre_hook = ts_addon.create_pre_hook() }
-
-    -- Improve Astro commenting and add support for Sass
-    local ft = require "Comment.ft"
-    ft.set("scss", { "//%s", "/*%s*/" })
-    ft.set("astro", { "<!--%s-->", "<!--%s-->" })
+    opts.pre_hook = ts_addon.create_pre_hook()
+    comment.setup(opts)
   end,
 }

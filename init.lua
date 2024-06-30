71121af6ee21d296981acd930ca71785f7efdb26
mgastonportillo <mgastonportillo@gmail.com>
2024-06-30 02:32:03 -0300

    chore: update init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,8 @@
 require "gale.globals"
 require "bootstrap"
-
 require "gale.usercmds"
 require "gale.autocmds"
 require "gale.aliases"
 require "gale.tabby"
 require "gale.wsl"
-
 require "mappings"

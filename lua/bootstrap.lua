61a8924b903ad0922cb09f3c76bc05cd6b180937
mgastonportillo <mgastonportillo@gmail.com>
2024-07-12 18:24:36 -0300

    feat(lazy): match the recommended Lazy bootstrapping code

--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -5,13 +5,19 @@ local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 -- Install lazy if not in path
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.system { "git", "clone", "--filter=blob:none", lazyrepo, "--branch=stable", lazypath }
+  local out = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
--- Prepend lazy to runtime path
 vim.opt.rtp:prepend(lazypath)
 
--- Load plugins
 local lazy_config = require "configs.lazy"
 require("lazy").setup({
   { "Bilal2453/luvit-meta" },

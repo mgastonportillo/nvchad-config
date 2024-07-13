a57b518b7b99b000fe53d8f3e4a4f28f6db2df22
mgastonportillo <mgastonportillo@gmail.com>
2024-07-13 14:18:45 -0300

    chore(spec): disable WhichKey notifications

--- lua/plugins/override/whichkey.lua
+++ lua/plugins/override/whichkey.lua
@@ -2,9 +2,7 @@ return {
   "folke/which-key.nvim",
   init = function()
     local map = vim.keymap.set
-
     map("n", "<leader>wK", "<cmd>WhichKey <CR>", { desc = "WhichKey show all keymaps" })
-
     map("n", "<leader>wk", function()
       vim.cmd("WhichKey " .. vim.fn.input "WhichKey: ")
     end, { desc = "WhichKey query lookup" })
@@ -13,5 +11,6 @@ return {
     icons = {
       rules = false,
     },
+    notify = false,
   },
 }

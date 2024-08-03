6e5de3ffec359cfb5e362cce064842dbd9513ace
mgastonportillo <mgastonportillo@gmail.com>
2024-08-03 17:55:19 -0300

    feat(spec): add custom callout to markview plugin

--- lua/plugins/spec/markview.lua
+++ lua/plugins/spec/markview.lua
@@ -5,4 +5,27 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
+  config = function()
+    local markview = require "markview"
+    local defaults = markview.configuration
+
+    local custom_callouts = {
+      {
+        match_string = "BRIEF",
+        aliases = nil,
+        callout_preview = "󰦩 Brief",
+        callout_preview_hl = "MarkviewCol3Fg",
+        custom_title = true,
+        custom_icon = "󰦩 ",
+        border = "▋",
+        border_hl = "MarkviewCol3Fg",
+      },
+    }
+
+    for _, callout in ipairs(custom_callouts) do
+      table.insert(defaults.block_quotes.callouts, callout)
+    end
+
+    markview.setup(defaults)
+  end,
 }

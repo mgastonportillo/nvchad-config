09dc1acf513933df2e7dbe461194d1fa6180c885
mgastonportillo <mgastonportillo@gmail.com>
2024-08-15 13:52:15 -0300

    chore(spec): update markview's configuration

--- lua/plugins/spec/markview.lua
+++ lua/plugins/spec/markview.lua
@@ -1,31 +1,8 @@
 return {
   "OXY2DEV/markview.nvim",
-  ft = "markdown",
+  lazy = false,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    local markview = require "markview"
-    local defaults = markview.configuration
-
-    local custom_callouts = {
-      {
-        match_string = "BRIEF",
-        aliases = nil,
-        callout_preview = "󰦩 Brief",
-        callout_preview_hl = "MarkviewCol3Fg",
-        custom_title = true,
-        custom_icon = "󰦩 ",
-        border = "▋",
-        border_hl = "MarkviewCol3Fg",
-      },
-    }
-
-    for _, callout in ipairs(custom_callouts) do
-      table.insert(defaults.block_quotes.callouts, callout)
-    end
-
-    markview.setup(defaults)
-  end,
 }

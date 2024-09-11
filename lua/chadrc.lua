8163b9fa114202df1cc9281b1c1091a74e05fdce
mgastonportillo <mgastonportillo@gmail.com>
2024-09-11 20:58:44 -0300

    refactor: chadrc

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,174 +1,119 @@
 ---@diagnostic disable: missing-fields
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
+local modules = require("gale.chadconf").modules
 
 ---@type ChadrcConfig
-local M = {}
+return {
+  base46 = {
+    transparency = true,
+    theme = "bearded-arc",
+    theme_toggle = { "bearded-arc", "chadracula-evondev" },
 
-M.base46 = {
-  transparency = true,
-  theme = "bearded-arc",
-  theme_toggle = { "bearded-arc", "chadracula-evondev" },
+    hl_override = {
+      Comment = { italic = true },
+      ["@comment"] = { italic = true },
+      CursorLineNr = { fg = "yellow", bold = true },
+      LspInlayHint = { fg = "#4e5665", bg = "NONE" },
+      FloatTitle = { link = "Title" },
+      FloatBorder = { link = "TelescopeBorder" },
+      NvimTreeRootFolder = { link = "TelescopeBorder" },
+      NvimTreeGitDirty = { link = "NvimTreeNormal" },
+      ["@keyword"] = { italic = true },
+    },
 
-  hl_override = {
-    Comment = { italic = true },
-    ["@comment"] = { italic = true },
-    CursorLineNr = { fg = "yellow", bold = true },
-    LspInlayHint = { fg = "#4e5665", bg = "NONE" },
-    FloatTitle = { link = "Title" },
-    FloatBorder = { link = "TelescopeBorder" },
-    NvimTreeRootFolder = { link = "TelescopeBorder" },
-    NvimTreeGitDirty = { link = "NvimTreeNormal" },
-    ["@keyword"] = { italic = true },
-  },
+    hl_add = {
+      YankVisual = { link = "CursorColumn" },
+      LspInfoBorder = { fg = "#444c5b" },
+      WinBar = { bg = "NONE" },
+      WinBarNC = { bg = "NONE" },
+      DropBarMenuCurrentContext = { link = "Visual" },
+      St_HarpoonInactive = { link = "StText" },
+      St_HarpoonActive = { link = "St_LspHints" },
+      NvimTreeGitStagedIcon = { fg = "#a6e3a1" },
+      MarkviewLayer2 = { bg = "#171b21" },
+      MarkviewCode = { link = "MarkviewLayer2" },
+      HelpviewCode = { link = "MarkviewLayer2" },
+      HelpviewInlineCode = { link = "MarkviewInlineCode" },
+      HelpviewCodeLanguage = { link = "MarkviewCode" },
+      ["@markup.quote.markdown"] = { bg = "NONE" },
+      ["@markup.raw.block.markdown"] = { link = "MarkviewLayer2" },
+    },
 
-  hl_add = {
-    YankVisual = { link = "CursorColumn" },
-    LspInfoBorder = { fg = "#444c5b" },
-    WinBar = { bg = "NONE" },
-    WinBarNC = { bg = "NONE" },
-    DropBarMenuCurrentContext = { link = "Visual" },
-    St_HarpoonInactive = { link = "StText" },
-    St_HarpoonActive = { link = "St_LspHints" },
-    NvimTreeGitStagedIcon = { fg = "#a6e3a1" },
-    MarkviewLayer2 = { bg = "#171b21" },
-    MarkviewCode = { link = "MarkviewLayer2" },
-    HelpviewCode = { link = "MarkviewLayer2" },
-    HelpviewInlineCode = { link = "MarkviewInlineCode" },
-    HelpviewCodeLanguage = { link = "MarkviewCode" },
-    ["@markup.quote.markdown"] = { bg = "NONE" },
-    ["@markup.raw.block.markdown"] = { link = "MarkviewLayer2" },
-  },
-
-  integrations = {
-    "blankline",
-    "cmp",
-    "codeactionmenu",
-    "dap",
-    "devicons",
-    "hop",
-    "lsp",
-    "mason",
-    "neogit",
-    "notify",
-    "nvimtree",
-    "rainbowdelimiters",
-    "semantic_tokens",
-    "todo",
-    "whichkey",
-  },
-}
-
-M.ui = {
-  telescope = { style = "bordered" },
-
-  tabufline = {
-    order = { "buffers", "tabs", "btns" },
-  },
-
-  statusline = {
-    theme = "vscode_colored",
-    order = {
-      "mode",
-      "tint",
-      "separator",
-      "file",
-      "git",
-      "%=",
-      "lsp_msg",
-      "%=",
-      "diagnostics",
+    integrations = {
+      "blankline",
+      "cmp",
+      "codeactionmenu",
+      "dap",
+      "devicons",
+      "hop",
       "lsp",
-      "harpoon",
-      "separator",
-      "cwd",
+      "mason",
+      "neogit",
+      "notify",
+      "nvimtree",
+      "rainbowdelimiters",
+      "semantic_tokens",
+      "todo",
+      "whichkey",
     },
-    modules = {
-      separator = " ",
-      hack = "%#@comment#%",
-      tint = "%#StText#", -- Force grey on modules that absorb neighbour colour
-      fill = "%#TbFill#%=",
-      modified = function()
-        return vim.bo.modified and " " or ""
-      end,
-      bufnr = function()
-        local bufnr = vim.api.nvim_get_current_buf()
-        return "%#StText#" .. tostring(bufnr)
-      end,
-      harpoon = function()
-        -- inspiration from https://github.com/letieu/harpoon-lualine
-        local inactive = "%#St_HarpoonInactive#"
-        local active = "%#St_HarpoonActive#"
+  },
 
-        local options = {
-          icon = active .. " ⇁ ",
-          separator = "",
-          indicators = {
-            inactive .. "q",
-            inactive .. "w",
-            inactive .. "e",
-            inactive .. "r",
-            inactive .. "t",
-            inactive .. "y",
-          },
-          active_indicators = {
-            active .. "1",
-            active .. "2",
-            active .. "3",
-            active .. "4",
-            active .. "5",
-            active .. "6",
-          },
-        }
+  ui = {
+    statusline = {
+      theme = "vscode_colored",
+      order = {
+        "mode",
+        "tint",
+        "separator",
+        "file",
+        "git",
+        "%=",
+        "lsp_msg",
+        "%=",
+        "diagnostics",
+        "lsp",
+        "harpoon",
+        "separator",
+        "cwd",
+      },
+      modules = {
+        hack = modules.statusline.hack,
+        harpoon = modules.statusline.harpoon,
+        separator = modules.statusline.separator,
+        tint = modules.statusline.tint,
+      },
+    },
 
-        local list = require("harpoon"):list()
-        local root_dir = list.config:get_root_dir()
-        local current_file_path = vim.api.nvim_buf_get_name(0)
-        local length = math.min(list:length(), #options.indicators)
-        local status = { options.icon }
+    tabufline = {
+      order = { "buffers", "tabs", "btns" },
+    },
 
-        local get_full_path = function(root, value)
-          if vim.loop.os_uname().sysname == "Windows_NT" then
-            return root .. "\\" .. value
-          end
+    cmp = {
+      style = "default",
+      icons = true,
+      lspkind_text = true,
+      icons_left = true,
+      format_colors = {
+        tailwind = true,
+        icon = "󱓻",
+      },
+    },
 
-          return root .. "/" .. value
-        end
+    telescope = { style = "bordered" },
+  },
 
-        for i = 1, length do
-          local value = list:get(i).value
-          local full_path = get_full_path(root_dir, value)
+  lsp = {
+    signature = true,
+    semantic_tokens = true,
+  },
 
-          if full_path == current_file_path then
-            table.insert(status, options.active_indicators[i])
-          else
-            table.insert(status, options.indicators[i])
-          end
-        end
-
-        if length > 0 then
-          table.insert(status, " ")
-          return table.concat(status, options.separator)
-        else
-          return ""
-        end
-      end,
+  term = {
+    float = {
+      border = "rounded",
+      height = 0.5,
+      width = 0.58,
+      col = 0.2,
+      row = 0.2,
     },
   },
 }
-
-M.lsp = {
-  signature = true,
-  semantic_tokens = true,
-}
-
-M.term = {
-  float = {
-    border = "rounded",
-    height = 0.5,
-    width = 0.58,
-    col = 0.2,
-    row = 0.2,
-  },
-}
-
-return M
---
+++ lua/gale/chadconf.lua
@@ -0,0 +1,86 @@
+local M = {}
+
+--- Show harpoon indicator in statusline
+local harpoon_statusline_indicator = function()
+  -- inspiration from https://github.com/letieu/harpoon-lualine
+  local inactive = "%#St_HarpoonInactive#"
+  local active = "%#St_HarpoonActive#"
+
+  local options = {
+    icon = active .. " ⇁ ",
+    separator = "",
+    indicators = {
+      inactive .. "q",
+      inactive .. "w",
+      inactive .. "e",
+      inactive .. "r",
+      inactive .. "t",
+      inactive .. "y",
+    },
+    active_indicators = {
+      active .. "1",
+      active .. "2",
+      active .. "3",
+      active .. "4",
+      active .. "5",
+      active .. "6",
+    },
+  }
+
+  local list = require("harpoon"):list()
+  local root_dir = list.config:get_root_dir()
+  local current_file_path = vim.api.nvim_buf_get_name(0)
+  local length = math.min(list:length(), #options.indicators)
+  local status = { options.icon }
+
+  local get_full_path = function(root, value)
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+      return root .. "\\" .. value
+    end
+
+    return root .. "/" .. value
+  end
+
+  for i = 1, length do
+    local value = list:get(i).value
+    local full_path = get_full_path(root_dir, value)
+
+    if full_path == current_file_path then
+      table.insert(status, options.active_indicators[i])
+    else
+      table.insert(status, options.indicators[i])
+    end
+  end
+
+  if length > 0 then
+    table.insert(status, " ")
+    return table.concat(status, options.separator)
+  else
+    return ""
+  end
+end
+
+M.modules = {
+  statusline = {
+    separator = " ", -- Add space between modules
+    hack = "%#@comment#%", -- Hack to make module highlight visible
+    tint = "%#StText#", -- Force grey on modules that absorb neighbour colour
+
+    modified = function()
+      return vim.bo.modified and " " or ""
+    end, -- Show modified indicator
+
+    bufnr = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+      return "%#StText#" .. tostring(bufnr)
+    end, -- Show current buffer number in statusline
+
+    harpoon = harpoon_statusline_indicator,
+  },
+
+  tabufline = {
+    fill = "%#TbFill#%=", -- Fill tabufline with TbFill hl
+  },
+}
+
+return M

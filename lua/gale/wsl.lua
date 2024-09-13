3e171c5546e4fa903b36fecbd6e0bf8a6ec82cfe
mgastonportillo <mgastonportillo@gmail.com>
2024-09-13 02:32:12 -0300

    refactor: wsl detection

--- lua/gale/wsl.lua
+++ lua/gale/wsl.lua
@@ -1,11 +1,15 @@
--- NOTE: WSL + WT padding recipe: "20,4,0,-4"
-if vim.fn.has "wsl" then
-  local copy = "clip.exe"
-  local paste = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
-  vim.g.clipboard = {
-    name = "wslclipboard",
-    copy = { ["+"] = copy, ["*"] = copy },
-    paste = { ["+"] = paste, ["*"] = paste },
-    cache_enabled = 0,
-  }
+if not vim.fn.has "wsl" then
+  return
 end
+
+-- set a global value to indicate that we are in WSL
+vim.g.is_wsl = true
+
+local copy = "clip.exe"
+local paste = 'pwsh.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
+vim.g.clipboard = {
+  name = "wslclipboard",
+  copy = { ["+"] = copy, ["*"] = copy },
+  paste = { ["+"] = paste, ["*"] = paste },
+  cache_enabled = 0,
+}

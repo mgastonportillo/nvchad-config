11aba7b95db49268932e133a93d99ac46c8b7cbb
mgastonportillo <mgastonportillo@gmail.com>
2024-09-18 12:03:54 -0300

    chore: remove unused alias

--- lua/gale/aliases.lua
+++ lua/gale/aliases.lua
@@ -1,9 +1,9 @@
 local alias = require("gale.utils").add_alias
 
+alias("CdProjectTab", "tcdp")
+
 -- Workarounds for my dumb fingers
 alias("qa", "Qa")
 alias("qa", "QA")
 alias("q", "Q")
 alias("w", "W")
-
-alias("CodeCompanion", "cc")

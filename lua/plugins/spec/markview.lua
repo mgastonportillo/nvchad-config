14119e8df0ad9f842b78f81166110da757d35b8f
mgastonportillo <mgastonportillo@gmail.com>
2024-09-18 12:06:43 -0300

    fix(spec): fix tables displaying incorrectly in markview

--- lua/plugins/spec/markview.lua
+++ lua/plugins/spec/markview.lua
@@ -6,4 +6,24 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
+  opts = {
+    modes = { "i", "n", "v", "vs", "V", "Vs", "no", "c" },
+    hybrid_modes = { "i" },
+    callbacks = {
+      on_enable = function(_, win)
+        -- https://github.com/OXY2DEV/markview.nvim/issues/75
+        vim.wo[win].wrap = false
+
+        -- https://segmentfault.com/q/1010000000532491
+        vim.wo[win].conceallevel = 2
+        vim.wo[win].concealcursor = "nivc"
+      end,
+    },
+    checkboxes = {
+      enable = true,
+    },
+    tables = {
+      use_virt_lines = true,
+    },
+  },
 }

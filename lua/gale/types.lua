b04b60a902b0b3a980145cda0df17fad6b29786d
mgastonportillo <mgastonportillo@gmail.com>
2024-09-25 18:56:43 -0300

    chore(spec): add type to be included in lazydev

--- lua/gale/types.lua
+++ lua/gale/types.lua
@@ -6,7 +6,10 @@ return {
     opts = {
       library = {
         { path = "luvit-meta/library", words = { "vim%.uv" } },
-        { path = vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types", words = { "ChadrcConfig", "NvPluginSpec" } },
+        {
+          path = vim.fn.stdpath "data" .. "/lazy/ui/nvchad_types",
+          words = { "ChadrcConfig", "NvPluginSpec", "Base46Table" },
+        },
         { path = "wezterm-types", mods = { "wezterm" } },
         -- vim.fn.expand "$HOME/workspace/neovim/ui/nvchad_types", -- get types when dev = true
       },

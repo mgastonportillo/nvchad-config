26f4063b894d3847dc37dc5ff4b5b334a73b36c2
mgastonportillo <mgastonportillo@gmail.com>
2024-10-03 00:02:19 -0300

    chore(spec): prefer NvChad color picker over CCC's

--- lua/plugins/spec/ccc.lua
+++ lua/plugins/spec/ccc.lua
@@ -6,7 +6,6 @@ return {
     local map = vim.keymap.set
     map("n", "cc", "<cmd>CccConvert<CR>", { desc = "Change Color space" })
     map("n", "ch", "<cmd>CccHighlighterToggle<CR>", { desc = "Toggle Color highlighter" })
-    map("n", "cp", "<cmd>CccPick<CR>", { desc = "Open Color picker" })
   end,
   config = function()
     require("ccc").setup {

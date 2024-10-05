37ef348cca4207fa17141592874d035b33cc23f1
mgastonportillo <mgastonportillo@gmail.com>
2024-10-05 02:58:31 -0300

    chore: move unused plugins to backup folder

--- lua/plugins/spec/neocomposer.lua
+++
@@ -1,9 +0,0 @@
----@type NvPluginSpec
-return {
-  enabled = false,
-  "ecthelionvi/NeoComposer.nvim",
-  dependencies = {
-    "kkharji/sqlite.lua",
-  },
-  opts = {},
-}
--- lua/plugins/spec/tabby-ml.lua
+++
@@ -1,8 +0,0 @@
-vim.g.tabby_keybinding_accept = "<Tab>"
-
----@type NvPluginSpec
-return {
-  "TabbyML/vim-tabby",
-  enabled = false,
-  lazy = false,
-}

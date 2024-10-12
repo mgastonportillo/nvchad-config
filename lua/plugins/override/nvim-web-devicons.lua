ce1548a8235810efb8d3dfa9d1e88cc0407604ad
mgastonportillo <mgastonportillo@gmail.com>
2024-10-12 04:58:17 -0300

    chore(spec): update .gitignore devicon style

--- lua/plugins/override/nvim-web-devicons.lua
+++ lua/plugins/override/nvim-web-devicons.lua
@@ -3,20 +3,19 @@ return {
   "nvim-tree/nvim-web-devicons",
   opts = {
     override = {
-      -- For some reason md changes here don't take effect
       md = { icon = "󰽛", color = "#FFFFFF", name = "Md" },
       mdx = { icon = "󰽛", color = "#FFFFFF", name = "Mdx" },
       markdown = { icon = "󰽛", color = "#FFFFFF", name = "Markdown" },
     },
     override_by_extension = {
-      astro = { icon = "", color = "#FE5D02", name = "astro" },
+      astro = { icon = "", color = "#FE5D02", name = "Astro" },
       javascript = { icon = "" },
       typescript = { icon = "󰛦" },
       lockb = { icon = "", color = "#FBF0DF", name = "bun-lock" },
     },
     override_by_filename = {
       [".stylua.toml"] = { icon = "", color = "#6D8086", name = "stylua" },
-      [".gitignore"] = { icon = "", color = "#6D8086", name = "gitignore" },
+      [".gitignore"] = { icon = "", color = "#F44D27", name = "gitignore" },
       ["license"] = { icon = "󰿃", name = "License" },
     },
   },

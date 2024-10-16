268950c0e211465e4f25c8f87a50ed985f6ded57
mgastonportillo <mgastonportillo@gmail.com>
2024-10-16 15:53:22 -0300

    chore(spec): reduce timeout in conform's format on save opts

--- lua/plugins/override/conform.lua
+++ lua/plugins/override/conform.lua
@@ -37,7 +37,7 @@ return {
       if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
         return
       end
-      return { timeout_ms = 10000, lsp_fallback = true }
+      return { timeout_ms = 5000, lsp_fallback = true }
     end,
     formatters = {
       yamlfmt = {

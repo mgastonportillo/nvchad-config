f2cc0c6d611fbd0b8b9eebd92160ed32c14e1dc0
mgastonportillo <mgastonportillo@gmail.com>
2024-10-18 02:36:36 -0300

    fix(spec): fix visual bug in help files caused by statuscol.nvim

--- lua/plugins/spec/statuscol.lua
+++ lua/plugins/spec/statuscol.lua
@@ -3,8 +3,21 @@ return {
   lazy = false,
   config = function()
     local builtin = require "statuscol.builtin"
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "help",
+      group = vim.api.nvim_create_augroup("HelpDocsBePure", { clear = true }),
+      callback = function()
+        if vim.o.ft == "help" then
+          vim.o.foldcolumn = "0"
+        else
+          vim.o.foldcolumn = "1"
+        end
+      end,
+    })
+
     require("statuscol").setup {
-      bt_ignore = { "terminal", "nofile" },
+      bt_ignore = { "terminal", "help", "nofile" },
       ft_ignore = { "oil" },
       relculright = true,
       segments = {

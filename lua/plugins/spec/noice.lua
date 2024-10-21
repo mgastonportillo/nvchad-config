37e840e2b57086a862ce8032912f4c77bc23b241
mgastonportillo <mgastonportillo@gmail.com>
2024-10-21 14:50:09 -0300

    chore(spec): add recommended values for noice

--- lua/plugins/spec/noice.lua
+++ lua/plugins/spec/noice.lua
@@ -1,9 +1,7 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-  },
+  dependencies = { "MunifTanjim/nui.nvim" },
   opts = {
     cmdline = {
       enabled = false,
@@ -22,6 +20,10 @@ return {
         enabled = true,
         opts = {
           border = "rounded",
+        },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
         },
       },
       signature = {

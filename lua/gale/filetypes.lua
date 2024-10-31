1aea6b0311e10475062ef6484ec8800dfd12d78a
mgastonportillo <mgastonportillo@gmail.com>
2024-10-31 03:29:08 -0300

    fix: jsonl custom check causing issues with some json files

--- lua/gale/filetypes.lua
+++ lua/gale/filetypes.lua
@@ -8,16 +8,22 @@ vim.api.nvim_create_autocmd("FileType", {
   desc = "Unattach jsonls from jsonl buffers.",
   pattern = "json",
   callback = function(args)
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if not client then
-      return
-    end
+    vim.schedule(function()
+      if not args.data then
+        return
+      end
 
-    local bufname = vim.api.nvim_buf_get_name(args.buf)
-    if client.name == "jsonls" and bufname:match "%.jsonl$" then
-      vim.schedule(function()
-        vim.lsp.stop_client(client.id)
-      end)
-    end
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if not client then
+        return
+      end
+
+      local bufname = vim.api.nvim_buf_get_name(args.buf)
+      if client.name == "jsonls" and bufname:match "%.jsonl$" then
+        vim.schedule(function()
+          vim.lsp.stop_client(client.id)
+        end)
+      end
+    end)
   end,
 })

a107e9f416fd45dd60526b36e46683d0c4342b2f
mgastonportillo <mgastonportillo@gmail.com>
2024-12-19 10:15:18 -0300

    chore(spec): update crates config to match latest version

--- lua/plugins/spec/crates.lua
+++ lua/plugins/spec/crates.lua
@@ -1,21 +1,24 @@
 ---@type NvPluginSpec
----@diagnostic disable: different-requires
 return {
   "saecki/crates.nvim",
-  ft = { "rust", "toml" },
-  dependencies = "nvim-lua/plenary.nvim",
-  config = function(_, opts)
+  event = { "BufRead Cargo.toml" },
+  tag = "stable",
+  opts = function(_, opts)
     local crates = require "crates"
-    local cmp = require "cmp"
-
-    crates.setup(opts)
-    cmp.setup.buffer {
-      sources = { { name = "crates" } },
-    }
-    crates.show()
 
     vim.keymap.set("n", "<leader>cu", function()
       crates.upgrade_all_crates()
     end, { desc = "Update crates" })
+
+    local options = {
+      completion = {
+        cmp = {
+          enabled = true,
+        },
+      },
+    }
+
+    opts = vim.tbl_deep_extend("force", opts or {}, options)
+    return opts
   end,
 }

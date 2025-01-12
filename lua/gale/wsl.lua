400c0af0db1205aca178d3478292ac00a499ac11
mgastonportillo <mgastonportillo@gmail.com>
2025-01-12 04:30:46 -0300

    chore: minor refactor

--- lua/gale/wsl.lua
+++ lua/gale/wsl.lua
@@ -5,8 +5,8 @@ end
 -- set a global value to indicate that we are in WSL
 vim.g.is_wsl = true
 
-local copy = vim.fn.expand "~" .. "/bin/win32yank.exe -i --crlf"
-local paste = vim.fn.expand "~" .. "/bin/win32yank.exe -o --lf"
+local copy = vim.fn.expand "$WIN" .. "/Utils/win32yank/win32yank.exe -i --crlf"
+local paste = vim.fn.expand "$WIN" .. "/Utils/win32yank/win32yank.exe -o --lf"
 vim.g.clipboard = {
   name = "wslclipboard",
   copy = { ["+"] = copy, ["*"] = copy },
